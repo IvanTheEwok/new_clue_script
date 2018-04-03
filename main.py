908e2d847c4d23648c66efe53a8f1e2ac2259e10
IvanTheEwok <ivan52x53@gmail.com>
2018-04-04 01:15:58 +0200

    Added some print statements to get around indent errors

--- main.py
+++ main.py
@@ -11,16 +11,22 @@ def main():
         elif action.lower() == "statistics":
             action_statistics = raw_input("What statstics would you like to see? [easy, medium, hard, elite, master, all]: ")
             if action_statistics.lower() == "easy":
+                print "Easy statistics:"
                 #Print easy statistics
             elif action_statistics.lower() == "medium":
+                print "Medium statistics:"
                 #Print medium statistics
             elif action_statistics.lower() == "hard":
+                print "Hard statistics:"
                 #Print hard statistics
             elif action_statistics.lower() == "elite":
+                print "Elite statistics:"
                 #Print elite statistics
             elif action_statistics.lower() == "master":
+                print "Master statistics:"
                 #Print master statistics
             elif action_statistics.lower() == "all":
+                print "All statistics:"
                 #Print all statistics
             else:    #Invalid input
                 print "Null"

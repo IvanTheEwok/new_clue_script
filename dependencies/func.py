66d2ab66511bf22d7ee86fed1fc72d59d36f701a
IvanTheEwok <ivan52x53@gmail.com>
2018-04-09 20:59:30 +0200

    Sanitized sql

--- dependencies/func.py
+++ dependencies/func.py
@@ -39,8 +39,8 @@ def read_tier(tier):
 
     sql = '''SELECT *
                 FROM clues
-                WHERE tier=tier'''
-    results = cursor.execute(sql)
+                WHERE tier=?'''
+    results = cursor.execute(sql,(tier))
     all_clues = results.fetchall()
     cursor.close()
     return all_clues    #A list of all the clues of a specific tier.
